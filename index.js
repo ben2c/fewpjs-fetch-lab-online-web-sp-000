9f21ceced86a58fb2d04494c6dd5df27ff3ef1fb
Benjamin Cheng <bencheng200@gmail.com>
2021-05-14 04:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => console.log(json));
+  .then(json => json.renderBooks());
+  
 }
 
 function renderBooks(books) {
